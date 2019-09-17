bde5bb584c47f7f9dc320c3b344b09dff1d91fef
bo <150469635@qq.com>
2019-09-17 21:10:07 +0800

    restore action.c from fifo

--- src/_ext/action.c
+++ src/_ext/action.c
@@ -1,8 +1,6 @@
 #include "project.h"
 #include "appweb.h"
 
-#define FIFO_PATH	"/tmp/pduc2appweb"
-
 const char   mainFrameDoctype[] =
 	"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n\n""<html xmlns=\"http://www.w3.org/1999/xhtml\">";
 
@@ -26,22 +24,17 @@ bool action_output_file(char *filepath, HttpQueue *q) {
 	FILE   *fd;
 	char   buf[1024];
 
-	fd = fopen(filepath, "r+");
+	fd = fopen(filepath, "r");
 	if (!fd) {
 		fd = fopen("/xprod/xweb/xfs/error.html", "r");
 		if (!fd)   return 0;
 	}
 	memset(buf, '\0', sizeof(buf));
-	int size;
-	mprLog("critical http auth", 3, "size@");
 	sleep(1);
 	while ((size = fread(buf, 1, sizeof(buf)/2-1, fd)) > 0) {
-		mprLog("critical http auth", 3, "size- %d", size);
-		mprLog("critical http auth", 3, "out::: %s", buf);
 		httpWrite(q, escapeNumberSign(buf));
 		memset(buf, '\0', sizeof(buf));
 	}
-	mprLog("critical http auth", 3, "size-- %d", size);
 	return 1;
 }
 
@@ -56,14 +49,7 @@ PUBLIC void action_get(HttpConn *conn)
 		sprintf(prm, "username=%s,/%s",conn->username, page);
 	else
 		return;
-	TICK ts, te;
-	tick_run();
-	ts = system_tick;
 	int ret = xif_sets(app_pduc, pduc_Oid_webRequest, 0, prm);
-	// tick_run();
-	te = system_tick;
-	mprLog("critical http auth", 3, "ts = %d, te = %d, ret = %d", ts, te, ret);
-
 	if (ret == 0) {
 		httpSetStatus(conn, 200);
 		httpWrite(q, mainFrameDoctype);
@@ -72,11 +58,9 @@ PUBLIC void action_get(HttpConn *conn)
 		 */		
 		httpAddHeaderString(conn, "Content-Type", "text/html");
 		httpSetHeaderString(conn, "Cache-Control", "no-cache");
-		mprLog("critical http auth", 3, "come--");
-		if (!action_output_file(/*"/tmp/boa/result.html"*/FIFO_PATH, q)) {
+		if (!action_output_file("/tmp/boa/result.html", q)) {
 			httpWrite(q, "file loss");
 		}
-		mprLog("critical http auth", 3, "--here");
 	   /*
 			Call finalize output and close the request.
 			Delay closing if you want to do asynchronous output and close later.
